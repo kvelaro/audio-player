1fdc1a2b8b4479b6bd85de69f6274fae0375146f
Kamil Velaro <kamilvelaro@gmail.com>
2022-03-08 16:58:54 +0300

    wip, refactor: AudioFile

--- src/ts/AudioFile.ts
+++ src/ts/AudioFile.ts
@@ -1,32 +1,15 @@
 import ProgressBar from "./ProgressBar";
-import AudioPlayerControls from "./AudioPlayerControls";
 
 export default class AudioFile {
     protected audioCtx: AudioContext
     protected selector: Element
     protected url: string
     protected audio: HTMLAudioElement
-    protected audioControls: AudioPlayerControls
     protected progressBar: ProgressBar
     constructor(selector: Element, url: string) {
 
         this.selector = selector
         this.url = url
-
-        this.audioControls = new AudioPlayerControls(this.selector, this.audioCtx)
-
-
-        //document.querySelector(this.selector).append(this.audio)
-
-
-        this.events()
-        this.progressBar = new ProgressBar(200)
-        //this.progressBar.drawAudio('http://localhost:8080/8d14102f3e5d724f1908.mp3')
-
-
-    }
-
-    protected events() {
 
     }
 
